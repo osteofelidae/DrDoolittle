eb1b2289cdc0916182508ddd175a004688537829
osteofelidae <azrieldelta@gmail.com>
2022-10-06 22:44:37 -0700

    First functional

--- DrDoolittle.py
+++ DrDoolittle.py
@@ -1,14 +1,44 @@
+KEYWORD = "dog"
+RESPONSE = "WOOF"
+
+
 import praw
 
 reddit = praw.Reddit(
     client_id="oghQpvu_5ScijfsD5rbTJA",
     client_secret="b5TGrTBn_wzoqQJqOzifRoPEydYWvA",
-    password="HanYang8176",
+    password=input("Input password... "),
     user_agent="DrDoolittle",
     username="osteofelidae"
 )
 
-print(reddit.user.me())
+try:
+    print("Logged in as " + reddit.user.me())
+except:
+    print("Authentication error.")
 
-subreddit = reddit.subreddit("BotsPlayHere")
-print(subreddit.description)
+try:
+    subreddit = reddit.subreddit(input("Enter subreddit... "))
+    print("Connected to r/" + subreddit.display_name)
+except:
+    print("Subreddit connection error.")
+
+try:
+    postlist = subreddit.new()
+    for submission in postlist:
+        postcontents = submission.selftext
+        posttitle = submission.title
+        
+        if ("dog" in postcontents.lower()) or ("dog" in posttitle.lower()):
+            
+            print("Found keyword " + KEYWORD + " in post titled: " + submission.title)
+            
+            commentidlist = submission.comments.list()
+            commented = False
+            for comment in commentidlist:
+                if comment.author == "osteofelidae":
+                    commented = True
+            if commented == False:
+                submission.reply(RESPONSE)
+except:
+    print("Access error.")
