d56b3bb8a3783183621514caf9e4799db677c94e
osteofelidae <azrieldelta@gmail.com>
2022-10-06 23:02:43 -0700

    Update DrDoolittle.py

--- DrDoolittle.py
+++ DrDoolittle.py
@@ -1,44 +1,35 @@
 KEYWORD = "dog"
 RESPONSE = "WOOF"
 
-
+import time
 import praw
 
 reddit = praw.Reddit(
-    client_id="oghQpvu_5ScijfsD5rbTJA",
-    client_secret="b5TGrTBn_wzoqQJqOzifRoPEydYWvA",
+    client_id="5sZupUPFN4GLN72MIxDhGg",
+    client_secret="	jLtqBp3Xh_NFKj_B0pBhjYav_5n3cg",
     password=input("Input password... "),
-    user_agent="DrDoolittle",
-    username="osteofelidae"
+    user_agent="WoofBot",
+    username="FindingDog"
 )
+time.sleep(1)
+print("Logged in as " + reddit.user.me())
 
-try:
-    print("Logged in as " + reddit.user.me())
-except:
-    print("Authentication error.")
+subreddit = reddit.subreddit(input("Enter subreddit... "))
+print("Connected to r/" + subreddit.display_name)
 
-try:
-    subreddit = reddit.subreddit(input("Enter subreddit... "))
-    print("Connected to r/" + subreddit.display_name)
-except:
-    print("Subreddit connection error.")
-
-try:
-    postlist = subreddit.new()
-    for submission in postlist:
-        postcontents = submission.selftext
-        posttitle = submission.title
+postlist = subreddit.new()
+for submission in postlist:
+    postcontents = submission.selftext
+    posttitle = submission.title
+    
+    if ("dog" in postcontents.lower()) or ("dog" in posttitle.lower()):
         
-        if ("dog" in postcontents.lower()) or ("dog" in posttitle.lower()):
-            
-            print("Found keyword " + KEYWORD + " in post titled: " + submission.title)
-            
-            commentidlist = submission.comments.list()
-            commented = False
-            for comment in commentidlist:
-                if comment.author == "osteofelidae":
-                    commented = True
-            if commented == False:
-                submission.reply(RESPONSE)
-except:
-    print("Access error.")
+        print("Found keyword " + KEYWORD + " in post titled: " + submission.title)
+        
+        commentidlist = submission.comments.list()
+        commented = False
+        for comment in commentidlist:
+            if comment.author == "FindingDog":
+                commented = True
+        if commented == False:
+            submission.reply(RESPONSE)
