1fd66324643bc4c62225f5cfaaebaaac803f6dd4
osteofelidae <azrieldelta@gmail.com>
2022-10-06 23:02:48 -0700

    Revert "Update DrDoolittle.py"
    This reverts commit d56b3bb8a3783183621514caf9e4799db677c94e.

--- DrDoolittle.py
+++ DrDoolittle.py
@@ -1,35 +1,44 @@
 KEYWORD = "dog"
 RESPONSE = "WOOF"
 
-import time
+
 import praw
 
 reddit = praw.Reddit(
-    client_id="5sZupUPFN4GLN72MIxDhGg",
-    client_secret="	jLtqBp3Xh_NFKj_B0pBhjYav_5n3cg",
+    client_id="oghQpvu_5ScijfsD5rbTJA",
+    client_secret="b5TGrTBn_wzoqQJqOzifRoPEydYWvA",
     password=input("Input password... "),
-    user_agent="WoofBot",
-    username="FindingDog"
+    user_agent="DrDoolittle",
+    username="osteofelidae"
 )
-time.sleep(1)
-print("Logged in as " + reddit.user.me())
 
-subreddit = reddit.subreddit(input("Enter subreddit... "))
-print("Connected to r/" + subreddit.display_name)
+try:
+    print("Logged in as " + reddit.user.me())
+except:
+    print("Authentication error.")
 
-postlist = subreddit.new()
-for submission in postlist:
-    postcontents = submission.selftext
-    posttitle = submission.title
-    
-    if ("dog" in postcontents.lower()) or ("dog" in posttitle.lower()):
+try:
+    subreddit = reddit.subreddit(input("Enter subreddit... "))
+    print("Connected to r/" + subreddit.display_name)
+except:
+    print("Subreddit connection error.")
+
+try:
+    postlist = subreddit.new()
+    for submission in postlist:
+        postcontents = submission.selftext
+        posttitle = submission.title
         
-        print("Found keyword " + KEYWORD + " in post titled: " + submission.title)
-        
-        commentidlist = submission.comments.list()
-        commented = False
-        for comment in commentidlist:
-            if comment.author == "FindingDog":
-                commented = True
-        if commented == False:
-            submission.reply(RESPONSE)
+        if ("dog" in postcontents.lower()) or ("dog" in posttitle.lower()):
+            
+            print("Found keyword " + KEYWORD + " in post titled: " + submission.title)
+            
+            commentidlist = submission.comments.list()
+            commented = False
+            for comment in commentidlist:
+                if comment.author == "osteofelidae":
+                    commented = True
+            if commented == False:
+                submission.reply(RESPONSE)
+except:
+    print("Access error.")
